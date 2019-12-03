0567f6ef1d6cf39eeb8484ad3ceb30760a2e17b5
Nate-pt <arqueirogamer72@gmail.com>
2019-12-03 14:54:05 -0300

    Build V3.2
    Add Menu de Pause

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+
+        else
+        {
+            DeactivateMenu();
+        }
+
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu() 
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
